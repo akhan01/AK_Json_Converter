b5e504bb2d49acd220ef1900700d5e3f39b515cc
Abrar Khan <Abrar.Khan@partner.bmw.co.uk>
2025-02-02 19:35:50 +0000

    Item Master implementation

---
+++ JsonConverter/Model/RawItemMasterBarcodeData.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonConverter.Model.ItemMaster
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class RawItemMasterBarcodeData
+    {
+        [JsonProperty("SKU code")]
+        public string SkuCode { get; set; }
+
+        [JsonProperty("Item Barcode")]
+        public string ItemBarcode { get; set; }
+
+        [JsonProperty("UOM Type")]
+        public string UOMType { get; set; }
+
+        [JsonProperty("Quantity per UOM")]
+        public int? QuantityperUOM { get; set; }
+    }
+}
